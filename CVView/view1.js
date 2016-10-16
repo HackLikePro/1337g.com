a157a55d8fa39aef713fe4084f1cc8ca77380b6e
hacklikepro <leetgu@gmail.com>
2016-10-15 21:12:07 -0400

    2 theme switch CV

--- CVView/view1.js
+++ CVView/view1.js
@@ -1,42 +1,51 @@
-
 angular.module('myApp.CVView', ['ngRoute'])
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/', {
-    templateUrl: 'CVView/Network_CV.html',
-    controller: 'NetWorkCtrl'
-  });
-}])
-    .factory('DataSource', ['$http',function($http){
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/', {
+            templateUrl: 'CVView/Network_CV.html',
+            controller: 'NetWorkCtrl'
+        });
+        $routeProvider.when('/1337', {
+            templateUrl: 'CVView/Network_CV2.html',
+            controller: 'NetWorkCtrl'
+        });
+    }])
+    .factory('DataSource', ['$http', function ($http) {
         return {
-            getData: function(file,callback,transform){
+            getData: function (file, callback, transform) {
                 $http.get(
                     file,
-                    {transformResponse:transform}
-                ).
-                success(function(data, status) {
+                    {transformResponse: transform}
+                ).success(function (data, status) {
                     console.log("Request succeeded");
                     callback(data);
-                }).
-                error(function(data, status) {
+                }).error(function (data, status) {
                     console.log("Request failed " + status);
                 });
             }
         };
     }])
-    .controller('NetWorkCtrl',['$scope','$http','DataSource',function($scope,$http,DataSource) {
-      var SOURCE_FILE = "1337g.xml";
-       xmlTransform = function(data) {
-        console.log("transform data");
-        var x2js = new X2JS();
-        var json = x2js.xml_str2json( data );
-        return json.profiles.profile;
-      };
+    .controller('NetWorkCtrl', ['$scope', '$http', '$location', '$timeout','DataSource', function ($scope, $http, $location, $timeout ,DataSource) {
+        var SOURCE_FILE = "1337g.xml";
+        xmlTransform = function (data) {
+            console.log("transform data");
+            var x2js = new X2JS();
+            var json = x2js.xml_str2json(data);
+            return json.profiles.profile;
+        };
 
-       setData = function(data) {
-        $scope.dataSet = data;
-      };
+        $scope.hideStuff = function () {
+            document.body.style.backgroundColor = 'black';
+            $scope.startFade = true;
+            to1337 = function(){$location.path('/1337');};
+            $timeout(to1337,2000);
+            DataSource.getData('1337.xml', setData, xmlTransform);
+        };
 
-      DataSource.getData(SOURCE_FILE,setData,xmlTransform);
+        setData = function (data) {
+            $scope.dataSet = data;
+        };
+
+        DataSource.getData(SOURCE_FILE, setData, xmlTransform);
 
     }]);
 
